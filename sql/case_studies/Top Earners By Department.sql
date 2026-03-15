ccba3655433340777f5a4f19c61227625e1be095
rabajpai <raksha.bajpai18@gmail.com>
2026-03-15 17:14:08 +0530

    Top Earners By Department.sql

--- sql/case_studies/Top Earners By Department.sql
+++ sql/case_studies/Top Earners By Department.sql
@@ -2,6 +2,8 @@
 --This query identifies the highest-paid employee in each department using a CTE and the RANK() window function.
 --Real-World Use Case:
 --Data analysts often need to highlight top performers or outliers in a dataset. For HR or finance teams, this query quickly surfaces the top earners per department for compensation analysis.
+--Using RANK() keeps ties so in case 2 employees in same department having highest salary it will show both Employee Names.
+
 
 WITH DeptSalaryRank AS (
     SELECT 
